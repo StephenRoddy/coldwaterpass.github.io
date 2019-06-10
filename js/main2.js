178106d0b485c455ecc6b70e665f97798515c5d1
Stephen Roddy <roddyst@tcd.ie>
2019-06-10 19:57:21 +0100

    Updated code (js) to imrove visuals
    Updated js code to improve visuals.
    CSS changes to ccentre button

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,7 @@
-var img, pieces, description, fs, radius, fft, peakDetect, audio, toggleBtn, uploadBtn, uploadedAudio, uploadAnim;
-var colorPalette = ["#E97611", "#8A63D3", "#7A2500", "#504623"];
+var img, pieces, r,g,b, description, fs, radius, fft, peakDetect, audio, toggleBtn, uploadBtn, uploadedAudio, uploadAnim;
+var colorPalette = ["#E97611", "#8A63D3", "#7A2500", "#504623", "#3F4B23", "#A04B82"]
+
+
 var uploadLoading = false;
 
 function preload() {
@@ -37,12 +39,12 @@ function setup() {
     toggleBtn.mousePressed(toggleAudio);
     toggleBtn.mousePressed(toggleBtn.hide);
     toggleBtn.mousePressed(fullscreen);
-
    // image(img, 10, 10, 50, 50);
 
-    loadImage('lib/HumanCost.jpg', img => {
-    image(img, 0, 0);
-  });
+
+// initialise background
+    background(colorPalette[1]); //Drop Image in here
+
 
   //  image(img, 0, 0); //album cover
 
@@ -54,7 +56,7 @@ function setup() {
    // audio.play();
 
  
-    pieces = 4;
+    pieces = 32;
     radius = windowHeight / 4;
 
     varianceX = 3;  
@@ -64,8 +66,6 @@ function setup() {
 //    peakDetect.onPeak(varyY);
 
 
-   // let description = 'Coldwater Pass is a data-driven composition that explores some of the human dimensions of Ireland’s economic crash focusing specifically on the relationship between poverty, drug crime, emigration, and suicide. It exploits the power of sound to re-embody the impersonal statistical data revealing aspects of the human realities underlying the cold hard facts. The piece uses a complex mapping strategy to map data that represents Deprivation Rate, Unemployment Rate, Emigration Rate, Drug Related Crime Rate and Annual Suicide Rate from 2007 to 2012 to musical features. This mapping manipulates patterns of tension and release in the musical material in order to communicate a sense of the human realities underlying the socioeconomic data. The piece is driven by a Csound algorithm that maps the data to vocal synthesis parameters defined by in Native Instruments Reaktor synthesis engine. Input data is rescaled and assigned to midi note, pan and CC data that is ported into Logic Pro X. GNP, Unemployment and Emigration Rate are mapped to create a background harmonic material while Deprivation rate and Drug Crime offenses create a type of foreground call and response pattern that is spatially distrusted with Drug Crime presented on the right and Deprivation rate on the left. All of this is underpinned by a rhythmic percussion pattern for which each hit indicates 60 suicides. Parameters such as vowel shape note length and formant shape are leveraged in the expression of the data through tension patterns.'
-   // text(description, 10, 10, 70, 80); // Text wraps within text box
 
 }
 
@@ -81,7 +81,7 @@ function draw() {
 
     */
 
-    background(colorPalette[0]); //Drop Image in here
+
 
     fft.analyze();
 
@@ -90,7 +90,9 @@ function draw() {
     var mid = fft.getEnergy("mid");
 
     var mapbass = map(bass, 0, 255, -100, 800);
-    var scalebass = map(bass, 0, 255, 0.5, 1.2);
+    var scalebass2 = map(bass, 0, 255, 0.5, 1.2);
+    var scalebass = map(bass, 0, 255, 0.01, .75);
+
     var radBass = map(bass, 0, 255, 10, 110);
     var modBass = map(bass, 0, 255, 0, 1);
 
@@ -103,6 +105,10 @@ function draw() {
 
     var mapTreble = map(treble, 0, 255, -radius / 4, radius * 4);
     var scaleTreble = map(treble, 0, 255, 1, 1.5);
+    var modTreb = map(treble, 0, 255, 0, 1);
+    var modColor = map(treble, 0, 255, 0, width);
+
+
 
    // peakDetect.update(fft);
   //  if ( peakDetect.isDetected ) {}
@@ -114,32 +120,69 @@ function draw() {
     translate(width / 2, height / 2);
 
 
+//Update background
+
+	r = 182;
+	g = 67;
+	b = 5;
+
+	r,g,b = modColor/3;
+ 	background (r,g,b);
+ 	fill (148, 109, 191);
+
+
     for (i = 0; i < pieces; i += 0.01) {
 
         rotate(TWO_PI / pieces);
 
-        /*----------  BASS  ----------*/
+        /*----------  BASS 1 ----------*/
         push();
-        strokeWeight(5*modBass);
-        stroke(colorPalette[1]);
+        strokeWeight(2*modBass);
+        stroke(colorPalette[0]);
         scale(scalebass);
-        rotate(frameCount*2);// * -0.5);
-        line(mapbass, radius / 3, radius, radius);
-        line(-mapbass, -radius / 4, radius, radius);
+        rotate(.01);// * -0.5);
+        line(mapbass, radius , radius, radius);
+       // line(-mapbass,radius*.5 , radius*.5, radius*.5);
+        pop();
+
+
+        /*----------  BASS 2 ----------*/
+
+        push();
+        strokeWeight(3*modBass);
+        stroke(colorPalette[2]);
+        scale(scalebass);
+        rotate(.03);// * -0.5);
+       // line(mapbass, radius , radius, radius);
+        line(-mapbass*.5,radius*.5 , radius*.5, radius*.5);
+        pop();
+
+
+        /*----------  BASS 3 ----------*/
+
+        push();
+        strokeWeight(4*modBass);
+        stroke(colorPalette[3]);
+        scale(scalebass);
+        rotate(.05);// * -0.5);
+       // line(mapbass, radius , radius, radius);
+        line(-mapbass*.75,radius*.75 , radius*.75, radius*.75);
+       // directionalLight(colorPalette[0], 3, 3, 5);
         pop();
 
 
         /*----------  MID  ----------*/
         push();
         strokeWeight(5*modMid);
-        stroke(colorPalette[2]);
+        stroke(colorPalette[4]);
         line(mapMid, radius, radius * 1.5, radius * 2.5);
         pop();
 
 
         /*----------  TREMBLE  ----------*/
         push();
-        stroke(colorPalette[3]);
+        strokeWeight(8*modTreb);
+        stroke(colorPalette[5]);
         scale(scaleTreble);
         line(mapTreble, radius / 4.8, radius, radius);
         pop();
@@ -147,7 +190,6 @@ function draw() {
     }
 
 }
-
 
 
 function toggleAudio() {
